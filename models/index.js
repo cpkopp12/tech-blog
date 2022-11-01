1d8d1b7361a9343fec5c6759f4c28bbc96a3d4f2
cpkopp <cameronpkopp@gmail.com>
2022-10-31 17:20:42 -1000

    post model set, next post routes

--- models/index.js
+++ models/index.js
@@ -1,5 +1,16 @@
 //DECLARATIONS: USER ----------------
 const User = require('./User');
+const Post = require('./Post');
 
-//EXPORT MODEL OBJECT
-module.exports = { User };
+//MODEL RELATIONS ================================
+//user to post
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+
+Post.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+//EXPORT MODEL OBJECT ---------------
+module.exports = { User, Post };
